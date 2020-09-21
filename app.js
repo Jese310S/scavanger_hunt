e92886cc4b24e41950fd365cf30210ff10895955
Jese310S <jess.sole710@gmail.com>
2020-09-21 15:09:05 +1000

    add map scripts and html for google map api

--- app.js
+++ app.js
@@ -41,3 +41,15 @@ function getItems() {
 //      `).join('');
 //      spellList.innerHTML = options;
 // }
+
+//---------------------------------
+//FUNCTIONS FOR MAP API
+//--------------------------------
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
