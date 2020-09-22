7fbf478c0a55f37af1b6435bdd3a9e68ec5ea62a
Hugh-Charlton <62679235+Hugh-Charlton@users.noreply.github.com>
2020-09-23 09:25:02 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ function getItems() {
             console.log(counter)
 
             for (i = 0; i < num; i++) {
+
+                // prevents infinte loops and extension of spell list temp fix
+                if (num > 20 || num * counter > 151 || mySpellbook.length > 9) {
+                  console.log("bad luck!")
+                  break;
+                }
                 randNum = randomNumber()
 
                 // adds a random spell to the list and then checks/removes dupes
@@ -37,12 +43,6 @@ function getItems() {
                 spellDesc = spellDesc.filter((v, i) => spellDesc.indexOf(v) === i)
                 console.log(mySpellbook)
                 console.log(spellDesc)
-
-                // prevents infinte loops
-                if (num > 20 || num * counter > 151) {
-                    console.log("bad luck!")
-                    break;
-                }
 
                 // removes 1 from i to make a loop not count when a dupe is created
                 if (i * counter > mySpellbook.length - (1 * counter)){
@@ -67,6 +67,74 @@ function getItems() {
         // const spellList = document.querySelector('.spellList').innerHTML
         // spellList.innerHTML = `<li>${mySpellbook[counter]}</li>`
         spellList.innerHTML = test
+
+
+        var iconBase =
+        'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+        var img = document.createElement("img");
+        img.src = "./bolt-3.png"
+  
+  
+        var icons = {
+            HP_map: {
+              icon: img.src
+            }
+          };
+  
+        var features = [
+          { /* state library */
+            position: new google.maps.LatLng(-37.80959, 144.965),
+            type: 'HP_map'
+          }, { /* chinatown */
+            position: new google.maps.LatLng(-37.81120, 144.96882),
+            type: 'HP_map'
+          }, { /* marvel stadium */
+            position: new google.maps.LatLng(-37.81636, 144.94754),
+            type: 'HP_map'
+          }, { /* queen vic market */
+            position: new google.maps.LatLng(-37.80736, 144.95678),
+            type: 'HP_map'
+          }, {/* st. pauls csathedral */
+            position: new google.maps.LatLng(-37.81681, 144.96763),
+            type: 'HP_map'
+          }, { /* ice bar */
+            position: new google.maps.LatLng(-37.81731, 144.96982),
+            type: 'HP_map'
+          }, {/* aquarium */
+            position: new google.maps.LatLng(-37.82052, 144.95913),
+            type: 'HP_map'
+          }, { /* ACMI */
+            position: new google.maps.LatLng(-37.81689, 144.96911),
+            type: 'HP_map'
+          }, { /* fed square */
+            position: new google.maps.LatLng(-37.81776, 144.96907),
+            type: 'HP_map'
+          }, { /* flagstaff */
+            position: new google.maps.LatLng(-33.91666, 151.23468),
+            type: 'HP_map'
+          }
+        ];
+      
+        // Create markers.
+  
+        for (var i = 0; i < features.length; i++) {
+  
+          // let contentString = `${mySpellbook[i]}`
+          
+          // infoWindow = new google.maps.InfoWindow({
+          //    content: contentString
+          // })
+      
+          marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map,
+            title: `${mySpellbook[i]}`
+          });
+          // marker.addListener('click', function(){
+          //   infoWindow.open(map, marker)
+          // })
+        };
     })
 }
 
@@ -74,8 +142,6 @@ function checked(spell) {
     document.getElementById(spell).style.background = "#3CB371"
     console.log(spell)
 }
-
-
 
 // function generateOptions(data) {
 //      console.log(data)
@@ -103,59 +169,4 @@ function initMap() {
   map = new google.maps.Map(
       document.getElementById('map-container-google-1'),
       {center: new google.maps.LatLng(-37.814, 144.964), zoom: 16});
-
-  var iconBase =
-      'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
-      var img = document.createElement("img");
-      img.src = "./bolt-3.png"
-
-
-var icons = {
-    HP_map: {
-      icon: img.src
-    }
-  };
-
-  var features = [
-    { /* state library */
-      position: new google.maps.LatLng(-37.80959, 144.965),
-      type: 'HP_map'
-    }, { /* chinatown */
-      position: new google.maps.LatLng(-37.81120, 144.96882),
-      type: 'HP_map'
-    }, { /* marvel stadium */
-      position: new google.maps.LatLng(-37.81636, 144.94754),
-      type: 'HP_map'
-    }, { /* queen vic market */
-      position: new google.maps.LatLng(-37.80736, 144.95678),
-      type: 'HP_map'
-    }, {/* st. pauls csathedral */
-      position: new google.maps.LatLng(-37.81681, 144.96763),
-      type: 'HP_map'
-    }, { /* ice bar */
-      position: new google.maps.LatLng(-37.81731, 144.96982),
-      type: 'HP_map'
-    }, {/* aquarium */
-      position: new google.maps.LatLng(-37.82052, 144.95913),
-      type: 'HP_map'
-    }, { /* ACMI */
-      position: new google.maps.LatLng(-37.81689, 144.96911),
-      type: 'HP_map'
-    }, { /* fed square */
-      position: new google.maps.LatLng(-37.81776, 144.96907),
-      type: 'HP_map'
-    }, { /* flagstaff */
-      position: new google.maps.LatLng(-33.91666, 151.23468),
-      type: 'HP_map'
-    }
-  ];
-
-  // Create markers.
-  for (var i = 0; i < features.length; i++) {
-    var marker = new google.maps.Marker({
-      position: features[i].position,
-      icon: icons[features[i].type].icon,
-      map: map
-    });
-  };
 }
